4f9bc18c3c4b8d3c966615118c34b59083cebd86
Brandon Ros <brandonros1@gmail.com>
2017-12-10 01:52:06 -0500

    request-promise to scrape, cheerio to parse, lowdb to store

---
+++ getBets.js
@@ -0,0 +1,46 @@
+var rp = require('request-promise');
+var cheerio = require('cheerio');
+var moment = require('moment');
+
+module.exports = async function() {
+  var options = {
+      uri: 'https://sports.bovada.lv/football/nfl',
+      transform: function (body) {
+          return cheerio.load(body);
+      }
+  };
+
+  var $ = await rp(options);
+
+  var scripts = $('script');
+
+  var matchingScriptHtml = scripts.toArray().map(function(script) {
+    return $(script).html();
+  }).find(function(html) {
+    return html.indexOf('swc_market_lists') !== -1;
+  });
+
+  if (!matchingScriptHtml) {
+    throw new Error('Script not found');
+  }
+
+  var games = JSON.parse(matchingScriptHtml.replace('var swc_market_lists = ', '')).items[0].itemList.items;
+
+  var bets = [];
+
+  games.forEach(function(game) {
+    game.displayGroups[0].itemList.forEach(function(bet) {
+      bet.outcomes.forEach(function(outcome) {
+        bets.push({
+          game: game.description,
+          startTime: moment.unix(game.startTime / 1000).format(),
+          bet: bet.description,
+          outcome: outcome.description,
+          price: outcome.price.american
+        });
+      });
+    });
+  });
+
+  return bets;
+};
---
+++ run.js
@@ -0,0 +1,51 @@
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var moment = require('moment');
+var hash = require('object-hash');
+
+var getBets = require('./getBets');
+
+(async function() {
+  var adapter = new FileSync('db.json');
+  var db = low(adapter);
+
+  db.defaults({ bets: [], changes: [] })
+    .write();
+
+  var bets = await getBets();
+
+  bets.forEach(function(bet) {
+    var betHash = hash({
+      game: bet.game,
+      startTime: bet.startTime,
+      bet: bet.bet,
+      outcome: bet.outcome
+    });
+
+    var dbBet = db.get('bets')
+      .find({ id: betHash })
+      .value();
+
+    if (!dbBet) {
+      console.log(new Date(), 'Inserting', betHash, bet);
+
+      db.get('bets')
+        .push(Object.assign({}, bet, { scraped: moment().format(), id: betHash }))
+        .write();
+    } else {
+      if (dbBet.price !== bet.price) {
+        console.log(new Date(), 'Updating', betHash, dbBet, bet);
+
+        db.get('changes')
+          .push({ betHash: betHash, oldPrice: dbBet.price, newPrice: bet.price, changed: moment().format() })
+          .write();
+
+        db.get('bets')
+          .find({ id: betHash })
+          .assign({ price: bet.price, scraped: moment().format() })
+          .write();
+      }
+    }
+  });
+})();
+
